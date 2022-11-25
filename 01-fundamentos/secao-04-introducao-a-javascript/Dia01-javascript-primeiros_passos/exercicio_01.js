d3e2ec2d5cbe043173833c241af2b9e5210ee5f2
Gustavo <gustavo.silvarib@gmail.com>
2022-11-25 09:13:40 -0300

    atualizando repositorio

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/Dia01-javascript-primeiros_passos/exercicio_01.js
@@ -0,0 +1,15 @@
+// 1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+// Adição
+// Subtração
+// Multiplicação
+// Divisão
+// Módulo
+
+const a = 10;
+const b = 5;
+
+console.log('Soma: ' + (a + b));
+console.log('Subtração: ' + (a - b));
+console.log('Multiplicação: ' + (a * b));
+console.log('Divisão: ' + (a / b));
+console.log('Módulo: ' + (a % b));
